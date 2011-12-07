e544508a21bfb48c10d3221b8138a3ae40cf104c
Dave Reisner <dreisner@archlinux.org>
2011-12-06 23:28:12 -0500

    remove unnecessary headers

--- wakeup.c
+++ wakeup.c
@@ -20,11 +20,9 @@
 #include <errno.h>
 #include <getopt.h>
 #include <pthread.h>
-#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
 #include <sys/wait.h>
 #include <time.h>
 #include <unistd.h>
